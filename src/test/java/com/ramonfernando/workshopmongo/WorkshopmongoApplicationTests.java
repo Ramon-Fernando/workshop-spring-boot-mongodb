97842565c67e1281cfc7f50d02dbc849236496cc
Ramon Fernando <ramon.fernando98@gmail.com>
2023-08-28 22:16:48 -0300

    Project created

---
+++ src/test/java/com/ramonfernando/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramonfernando.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
