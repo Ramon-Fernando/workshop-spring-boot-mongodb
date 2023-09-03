6f7862d26e862ea45b1d9c92474c0a02e767deb1
Ramon Fernando <ramon.fernando98@gmail.com>
2023-09-03 20:58:58 -0300

    Creating entity Post with nested User

---
+++ src/main/java/com/ramonfernando/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.ramonfernando.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ramonfernando.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository <Post, String> {
+}
