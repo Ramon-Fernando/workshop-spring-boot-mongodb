9578435400a86df7cc96c5e7b20a38cac63a87ff
Ramon Fernando <ramon.fernando98@gmail.com>
2023-09-03 21:41:29 -0300

    Getting a post by id

---
+++ src/main/java/com/ramonfernando/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.ramonfernando.workshopmongo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ramonfernando.workshopmongo.domain.Post;
+import com.ramonfernando.workshopmongo.repository.PostRepository;
+import com.ramonfernando.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Post user = repo.findById(id).get();
+		if (user == null) {
+			throw new ObjectNotFoundException("Object not found");
+		}
+		return user;
+	}
+	
+}
